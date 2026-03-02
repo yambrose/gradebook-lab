17a229fdb75f22b1a0156cd34309980419aaa408
yambrose <randomskitztm@gmail.com>
2026-03-02 01:02:57 -0500

    nah we restarting

--- src/js/gradebook.js
+++ src/js/gradebook.js
@@ -1,64 +1,54 @@
 // src/js/gradebook.js
 // Data + parsing + utilities for Gradebook Explorer
 
+const table = $("#grades-table");
+const tableBody = $("#table-body");
+
 const testData = {
     headers: ["Student", "Lab 1", "Lab 2", "Midterm", "Final"],
     students: ["Johnson", "Adam Tam", "Suryansh Saroch"],
-    marks: [[12, 23, 42, 21], [34, 12, 22, 33], [23, 34, 12, 11]]
+    grades: [[12, 23, 42, 21], [34, 12, 22, 33], [23, 34, 12, 11]]
 }
 
-function fillGradesTable() {
-    $("#header-row").append(testData.headers.map((header, index) => `<th id="header-${index}">${header}</th>`).join(""));
-    testData.headers.forEach((header, index) => {
-        if (index > 0) {
-            $(`#header-${index}`).click(() => selectAssignment(index));
-        }
-    });
+function fillTableData() {
+    // Fill header
+    const headerRow = $("#header-row");
+    for (let i = 0; i < testData.headers.length; i++) {
+        const header = $(`<th>${testData.headers[i]}</th>`);
+        if (i > 0) { header.on("click", selectCol) }
+        headerRow.append(header);
+
+    }
+
+    // Fill body
     for (let i = 0; i < testData.students.length; i++) {
         const student = testData.students[i];
-        const marks = testData.marks[i];
-        $("#grades-table").append(`
-            <tr class="data-row">
-                <td id="student-${i}" class="student-name">${student}</td>
-                ${marks.map(mark => `<td id="${student}-${headers[i]}">${mark}</td>`).join("")}
+        const grades = testData.grades[i];
+        tableBody.append(
+            `<tr>
+                <td class="student-name">${student}</td>
+                ${grades.map((grade) => `<td class="grade-cell">${grade}</td>`).join("")}
             </tr>
-        `);
-        $(`${student}-${headers[i]}`).click(() => selectCell(i));
-        $(".student-name").last().click(() => selectStudent(i));
+        `)
+        $(`tr:last-child .student-name`).on("click", selectRow);
     }
 }
 
-function selectStudent(studentIndex) {
-    console.log(`Selected student: ${testData.students[studentIndex]}`);
-    deselectAll();
-    const currentRow = $(`#student-${studentIndex}`).parent();
-    for (let i = 0; i < testData.headers.length - 1; i++) {
-        const cell = currentRow.children().eq(i + 1);
-        cell.addClass("selected");
-    }
-    updateSummary()
+function readSelectedData() {
+    const selected = $(".selected").get();
+    console.log(selected);
 }
 
-function selectAssignment(assignmentId) {
-    console.log(`Selected assignment: ${testData.headers[assignmentId]}`);
-    deselectAll();
-    for (let i = 0; i < testData.students.length; i++) {
-        const cell = $(`#student-${i}`).parent().children().eq(assignmentId);
-        cell.addClass("selected");
-    }
+function selectRow() {
+    console.log("asdad")
+}
+
+function selectCol() {
+    console.log("asdad")
+}
+
+function editCell() {
 
 }
 
-function selectCell(id) {
-    console.log(`Editing cell for ${testData.students[i]}: ${testData.headers[i]}`);
-}
-
-function deselectAll() {
-    $(".selected").removeClass("selected");
-}
-
-function updateSummary() {
-    //TODO
-}
-
-fillGradesTable()
+fillTableData()
