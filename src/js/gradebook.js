b9d918e687a9c2f5c14160183d2031d6c71e768d
ambrose <112982844+yambrose@users.noreply.github.com>
2026-03-02 16:37:00 -0500

    update

--- src/js/gradebook.js
+++ src/js/gradebook.js
@@ -7,7 +7,7 @@ const tableBody = $("#table-body");
 const testData = {
     headers: ["Student", "Lab 1", "Lab 2", "Midterm", "Final"],
     students: ["Johnson", "Adam Tam", "Suryansh Saroch"],
-    grades: [[12, 23, 42, 21], [34, 12, 22, 33], [23, 34, 12, 11]]
+    grades: [["12", "23", "42", "21"], ["34", "12", "22", "33"], ["23", "34", "fake", "11"]]
 }
 
 function fillTableData() {
@@ -15,7 +15,10 @@ function fillTableData() {
     const headerRow = $("#header-row");
     for (let i = 0; i < testData.headers.length; i++) {
         const header = $(`<th>${testData.headers[i]}</th>`);
-        if (i > 0) { header.on("click", () => selectCol(i-1)) }  // -1 because Student header shouldn't count
+        if (i > 0) {
+            header.on("click", () => selectCol(i-1)) // -1 because Student header shouldn't count
+            header.addClass("clickable")
+        }
         headerRow.append(header);
     }
 
@@ -25,7 +28,7 @@ function fillTableData() {
         const grades = testData.grades[i];
         tableBody.append(
             `<tr>
-                <td class="student-name">${student}</td>
+                <td class="student-name clickable">${student}</td>
                 ${grades.map((grade, index) => `<td class="col-${index} row-${i}">${grade}</td>`).join("")}
             </tr>
         `)
@@ -34,8 +37,12 @@ function fillTableData() {
 }
 
 function readSelectedData() {
-    const selected = $(".selected").toArray().map((item) => item.textContent);
-    console.log(selected);
+    return $(".selected").toArray().map((item) => {
+        console.log(item.textContent)
+        if (/^\d+/.test(item.textContent)) {
+            return parseFloat(item.textContent);
+        }
+    }).filter(Boolean);
 }
 
 function deselectAll() {
@@ -46,18 +53,30 @@ function selectRow(rowId) {
     console.log(`Selecting row ${rowId}.`);
     deselectAll();
     $(`.row-${rowId}`).addClass("selected");
-    readSelectedData();
+    updateSummary();
 }
 
 function selectCol(colId) {
     console.log(`Selecting column ${colId}.`);
     deselectAll();
     $(`.col-${colId}`).addClass("selected");
-    readSelectedData();
+    updateSummary();
+}
+
+function updateSummary() {
+    const data = readSelectedData();
+    console.log(data)
+    const mean = data.reduce((a, b) => a+b, 0)/data.length;
+    const min = Math.min(data);
+    const max = Math.max(data);
+
+    $("#count").text(data.length);
+    $("#mean").text(mean.toFixed(2));
+
 }
 
 function editCell() {
-
+    claire
 }
 
 fillTableData()
