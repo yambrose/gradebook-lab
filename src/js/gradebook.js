4a6b515b23a1483bf99aa5da3de76e4078e39315
ambrose <112982844+yambrose@users.noreply.github.com>
2026-03-02 18:07:21 -0500

    update

--- src/js/gradebook.js
+++ src/js/gradebook.js
@@ -3,10 +3,11 @@
 
 const table = $("#grades-table");
 const tableBody = $("#table-body");
+let isEditing = false;
 
 const testData = {
     headers: ["Student", "Lab 1", "Lab 2", "Midterm", "Final"],
-    students: ["Johnson", "Adam Tam", "Suryansh Saroch"],
+    students: ["Johnson", "Adam Tam", "Suryansh"],
     grades: [["12", "23", "42", "21"], ["34", "12", "22", "33"], ["23", "34", "fake", "11"]]
 }
 
@@ -16,7 +17,7 @@ function fillTableData() {
     for (let i = 0; i < testData.headers.length; i++) {
         const header = $(`<th>${testData.headers[i]}</th>`);
         if (i > 0) {
-            header.on("click", () => selectCol(i-1)) // -1 because Student header shouldn't count
+            header.on("click", () => selectColumn(i-1)) // -1 because Student header shouldn't count
             header.addClass("clickable")
         }
         headerRow.append(header);
@@ -29,9 +30,10 @@ function fillTableData() {
         tableBody.append(
             `<tr>
                 <td class="student-name clickable">${student}</td>
-                ${grades.map((grade, index) => `<td class="col-${index} row-${i}">${grade}</td>`).join("")}
+                ${grades.map((grade, index) => `<td class="col-${index} row-${i} cell">${grade}</td>`).join("")}
             </tr>
         `)
+        $(`tr:last-child .cell`).on("click", editCell);
         $(`tr:last-child .student-name`).on("click",() => selectRow(i));
     }
 }
@@ -53,17 +55,17 @@ function selectRow(rowId) {
     console.log(`Selecting row ${rowId}.`);
     deselectAll();
     $(`.row-${rowId}`).addClass("selected");
-    updateSummary();
+    updateSummary("Row", testData.students[rowId]);
 }
 
-function selectCol(colId) {
+function selectColumn(colId) {
     console.log(`Selecting column ${colId}.`);
     deselectAll();
     $(`.col-${colId}`).addClass("selected");
-    updateSummary();
+    updateSummary("Column", testData.headers[colId+1]);
 }
 
-function updateSummary() {
+function updateSummary(direction, selectedName) {
     const data = readSelectedData();
     const hasSelection = data && data.length > 0;
 
@@ -71,7 +73,7 @@ function updateSummary() {
     const min = hasSelection ? Math.min(...data).toFixed(2) : 0;
     const max = hasSelection ? Math.max(...data).toFixed(2) : 0;
 
-    $("selected")
+    $("#selected").text(`${direction}: ${selectedName}`)
     $("#count").text(data.length);
     $("#mean").text(mean > 0 ? mean : 0);
     $("#min").text(min > 0 ? min : "-");
@@ -79,8 +81,37 @@ function updateSummary() {
 
 }
 
+function updateCell() {
+
+}
+
+function updateCell(newValue, originalValue) {
+    if (/^\d+$/.test(newValue) && newValue !== originalValue) {
+        //update the data and the csv
+    }
+}
+
+
+// This thing is buggy need to fix
 function editCell() {
-    claire
+    if (isEditing) return;
+    isEditing = true;
+    const cell = $(this);
+    const originalValue = cell.text();
+    const input = $("<input type='tel' id='edit-input'>")
+
+    input.on("keydown", (e) => {
+        const newValue = input.val().trim();
+        if (e.key === "Enter") {
+            updateCell(newValue, originalValue);
+            $("#edit-input").remove();
+            cell.text(newValue);
+            isEditing = false;
+        }
+    });
+    input.val(originalValue);
+    cell.text("").append(input);
+    input.focus();
 }
 
 fillTableData()
