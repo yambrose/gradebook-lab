5533b84207b55e76dbb9b809a34b27cf60823ee7
ambrose <112982844+yambrose@users.noreply.github.com>
2026-03-03 13:36:48 -0500

    progress

--- src/js/gradebook.js
+++ src/js/gradebook.js
@@ -8,28 +8,47 @@ let selectedDirection = null;
 let selectedName = null;
 let isEditing = false;
 
-const testData = {
-    headers: ["Student", "Lab 1", "Lab 2", "Midterm", "Final"],
-    students: ["Johnson", "Adam Tam", "Suryansh"],
-    grades: [["12", "23", "42", "21"], ["34", "12", "22", "33"], ["23", "34", "fake", "11"]]
+async function readDataFromCSV() {
+    let updatedData = {
+        headers: [],
+        students: [],
+        grades: []
+    };
+
+    await fetch("http://localhost:8000/src/data/grades.csv")
+        .then(response => response.text())
+        .then(data => data.trim().split('\n').map((row) => row.split(',')))
+        .then(data => {
+            console.log(data);
+            updatedData.headers = data[0];
+            for (let i = 1; i < data.length; i++) {
+                updatedData.students.push((data[i][0]))
+                updatedData.grades.push(data[i].slice(1));
+            }
+            fillTableData(updatedData);
+        }
+        )
+
+    console.log(updatedData);
+    return updatedData;
 }
 
-function fillTableData() {
+function fillTableData(data) {
     // Fill header
     const headerRow = $("#header-row");
-    for (let i = 0; i < testData.headers.length; i++) {
-        const header = $(`<th>${testData.headers[i]}</th>`);
+    for (let i = 0; i < data.headers.length; i++) {
+        const header = $(`<th>${data.headers[i]}</th>`);
         if (i > 0) {
-            header.on("click", () => selectColumn(i-1)) // -1 because Student header shouldn't count
+            header.on("click", () => selectColumn(i-1, data)) // -1 because Student header shouldn't count
             header.addClass("clickable")
         }
         headerRow.append(header);
     }
 
     // Fill body
-    for (let i = 0; i < testData.students.length; i++) {
-        const student = testData.students[i];
-        const grades = testData.grades[i];
+    for (let i = 0; i < data.students.length; i++) {
+        const student = data.students[i];
+        const grades = data.grades[i];
         tableBody.append(
             `<tr>
                 <td class="student-name clickable">${student}</td>
@@ -37,7 +56,7 @@ function fillTableData() {
             </tr>
         `)
         $(`tr:last-child .cell`).on("click", editCell);
-        $(`tr:last-child .student-name`).on("click",() => selectRow(i));
+        $(`tr:last-child .student-name`).on("click",() => selectRow(i, data));
     }
 }
 
@@ -54,48 +73,46 @@ function deselectAll() {
     $(".selected").removeClass("selected");
 }
 
-function selectRow(rowId) {
+function selectRow(rowId, data) {
     console.log(`Selecting row ${rowId}.`);
     deselectAll();
     $(`.row-${rowId}`).addClass("selected");
 
     selectedDirection = "Row";
-    selectedName = testData.students[rowId];
-    updateSummary("Row", testData.students[rowId]);
+    selectedName = data.students[rowId];
+    updateSummary("Row", data.students[rowId]);
 }
 
-function selectColumn(colId) {
+function selectColumn(colId, data) {
     console.log(`Selecting column ${colId}.`);
     deselectAll();
     $(`.col-${colId}`).addClass("selected");
     selectedDirection = "Column";
-    selectedName = testData.headers[colId+1];
+    selectedName = data.headers[colId+1];
     updateSummary();
 }
 
 function updateSummary() {
-    const data = readSelectedData();
-    const hasSelection = data && data.length > 0;
+    const selected = readSelectedData();
+    const hasSelection = selected && selected.length > 0;
 
-    const mean = hasSelection ? (data.reduce((a, b) => a+b, 0)/data.length).toFixed(2) : 0;
-    const min = hasSelection ? Math.min(...data).toFixed(2) : 0;
-    const max = hasSelection ? Math.max(...data).toFixed(2) : 0;
+    const mean = hasSelection ? (selected.reduce((a, b) => a+b, 0)/selected.length).toFixed(2) : 0;
+    const min = hasSelection ? Math.min(...selected).toFixed(2) : 0;
+    const max = hasSelection ? Math.max(...selected).toFixed(2) : 0;
 
     $("#selected").text(`${selectedDirection}: ${selectedName}`)
-    $("#count").text(data.length);
+    $("#count").text(selected.length);
     $("#mean").text(mean > 0 ? mean : 0);
     $("#min").text(min > 0 ? min : "-");
     $("#max").text(max > 0 ? max : "-");
 
 }
 
-function updateCell() {
-
-}
-
 function updateCell(cell, newValue, originalValue) {
-    if (/^\d+$/.test(newValue) && newValue !== originalValue) {
+    if (/^\d+$/.test(newValue) && newValue !== originalValue && newValue.length > 0) {
         cell.text(newValue);
+    } else {
+        cell.text(originalValue);
     }
     updateSummary();
 }
@@ -122,4 +139,10 @@ function editCell() {
     input.focus();
 }
 
-fillTableData()
+// TODO: Write the data to the csv, then reread it to update the table
+function writeDataToCSV() {
+
+}
+
+
+readDataFromCSV();
