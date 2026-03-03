1dea52838f4a84ff6b5a6aaf138ac765793b9f9c
ambrose <112982844+yambrose@users.noreply.github.com>
2026-03-03 16:18:43 -0500

    changes

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,2 +1,37 @@
 // src/js/chart.js
 // D3 charting for Gradebook Explorer
+
+function buildGraphData(selectedData) {
+    const total = selectedData.length;
+    if (total === 0) return {A:0, B:0, C:0, D:0, F:0};
+
+    let counts = {A: 0, B: 0, C: 0, D: 0, F: 0};
+
+    for (grade of selectedData) {
+        if (grade >= 90) counts.A++;
+        else if (grade >= 80) counts.B++;
+        else if (grade >= 70) counts.C++;
+        else if (grade >= 60) counts.D++;
+        else counts.F++;
+    }
+    console.log(counts);
+
+    return {
+        A: counts.A / total,
+        B: counts.B / total,
+        C: counts.C / total,
+        D: counts.D / total,
+        F: counts.F / total
+    }
+}
+
+function createBarChart(xLab, yLab, data) {
+    // Get the list of letter grades
+    const letterGrades = buildGraphData(data)
+    console.log(letterGrades);
+
+    // Count the frequency of each letter grade
+
+}
+
+createBarChart("Letter Grade", "Frequency", [85, 92, 78, 65, 55, 90, 82, 73, 88, 95]);
--- src/js/spreadsheet.js
+++ src/js/spreadsheet.js
@@ -1,14 +1,119 @@
 // src/js/spreadsheet.js
 // DOM generation + jQuery interactivity + editing + summary updates
 
-function readGradesData(path) {
-    //TODO
+
+const table = $("#grades-table");
+const tableBody = $("#table-body");
+
+let selectedDirection = null;
+let selectedName = null;
+let isEditing = false;
+
+function fillTableData(data) {
+    // Fill header
+    const headerRow = $("#header-row");
+    for (let i = 0; i < data.headers.length; i++) {
+        const header = $(`<th>${data.headers[i]}</th>`);
+        if (i > 0) {
+            header.on("click", () => selectColumn(i-1, data)) // -1 because Student header shouldn't count
+            header.addClass("clickable")
+        }
+        headerRow.append(header);
+    }
+
+    // Fill body
+    for (let i = 0; i < data.students.length; i++) {
+        const student = data.students[i];
+        const grades = data.grades[i];
+        tableBody.append(
+            `<tr>
+                <td class="student-name clickable">${student}</td>
+                ${grades.map((grade, index) => `<td class="col-${index} row-${i} cell">${grade}</td>`).join("")}
+            </tr>
+        `)
+        $(`tr:last-child .cell`).on("click", editCell);
+        $(`tr:last-child .student-name`).on("click",() => selectRow(i, data));
+    }
 }
 
+function readSelectedData() {
+    return $(".selected").toArray().map((item) => {
+        if (/^\d+/.test(item.textContent)) {
+            return parseFloat(item.textContent);
+        }
+    }).filter(Boolean);
+}
 
+function deselectAll() {
+    selectedDirection = null;
+    selectedName = null;
+    $(".selected").removeClass("selected");
+}
 
-const testStudents = [
-    {
+function selectRow(rowId, data) {
+    console.log(`Selecting row ${rowId}.`);
+    deselectAll();
+    $(`.row-${rowId}`).addClass("selected");
 
+    selectedDirection = "Row";
+    selectedName = data.students[rowId];
+    updateSummary("Row", data.students[rowId]);
+}
+
+function selectColumn(colId, data) {
+    console.log(`Selecting column ${colId}.`);
+    deselectAll();
+    $(`.col-${colId}`).addClass("selected");
+    selectedDirection = "Column";
+    selectedName = data.headers[colId+1];
+    updateSummary();
+}
+
+function updateSummary() {
+    const selected = readSelectedData();
+    const hasSelection = selected && selected.length > 0;
+
+    const mean = hasSelection ? (selected.reduce((a, b) => a+b, 0)/selected.length).toFixed(2) : 0;
+    const min = hasSelection ? Math.min(...selected).toFixed(2) : 0;
+    const max = hasSelection ? Math.max(...selected).toFixed(2) : 0;
+
+    $("#selected").text(`${selectedDirection}: ${selectedName}`)
+    $("#count").text(selected.length);
+    $("#mean").text(mean > 0 ? mean : 0);
+    $("#min").text(min > 0 ? min : "-");
+    $("#max").text(max > 0 ? max : "-");
+
+}
+
+function updateCell(cell, newValue, originalValue) {
+    if (/^\d+(?:\.+\d*)?$/.test(newValue) &&
+        newValue.length > 0 &&
+        parseFloat(newValue) >= 0 &&
+        newValue !== originalValue
+    ) {
+        cell.text(parseFloat(newValue));
+    } else {
+        cell.text(originalValue);
     }
-]
+    updateSummary();
+}
+
+function editCell() {
+    if (isEditing) return;
+    isEditing = true;
+    const cell = $(this);
+    const originalValue = cell.text();
+    const input = $("<input type='tel' id='edit-input'>")
+
+    input.on("keydown", (e) => {
+        const newValue = input.val().trim();
+        if (e.key === "Enter") {
+            updateCell(cell, newValue, originalValue);
+            $("#edit-input").remove();
+            isEditing = false;
+        }
+    });
+    input.val(originalValue);
+    cell.append(input);
+    input.focus();
+}
